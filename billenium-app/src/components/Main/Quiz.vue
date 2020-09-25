e67977f0970f991086be76661f631335c0c67380
jrkwsk <ula.jurkowska@gmail.com>
2020-09-25 12:06:17 +0200

    update quiz.vue

--- billenium-app/src/components/Main/Quiz.vue
+++ billenium-app/src/components/Main/Quiz.vue
@@ -1,13 +1,47 @@
 <template>
   <div class="container">
-    <h2 class>Question {{number}} / {{sumOfQuestions}}</h2>
-    <span :technology>{{technology.software.text}}</span>
-    <hr />
+    <div class="row header-line">
+      <div class="col-10">
+        <h2>Question {{questionNo}} / {{questions.length}}</h2>
+      </div>
 
-    <img src="..\..\assets\logo.png" alt />
-    <router-link to="/results">
+      <div class="col-2">
+        <span>{{ questions[questionNo-1].technology}}</span>
+      </div>
+    </div>
+    <hr />
+    <h3>{{questions[questionNo-1].text}}</h3>
+    <br />
+    <form action>
+      <br />
+      <input type="radio" />
+      <label for>{{questions[questionNo-1].answer1}}</label>
+      <br />
+      <input type="radio" />
+      <label for>{{questions[questionNo-1].answer2}}</label>
+      <br />
+      <input type="radio" />
+      <label for>{{questions[questionNo-1].answer3}}</label>
+      <br />
+      <input type="radio" />
+      <label for>{{questions[questionNo-1].answer4}}</label>
+
+      <br />
+    </form>
+    <div class="row footer-line">
+      <div class="col-3">
+        <img src="..\..\assets\logo.jpg" alt />
+      </div>
+      <div class="col-7">
+        <h3>Time left: 00:00:00</h3>
+      </div>
+      <div class="col-2">
+        <button class="btn btn-primary" @click="questionNo++">submit</button>
+      </div>
+    </div>
+    <!-- <router-link to="/results">
       <button class="btn btn-primary">go to results</button>
-    </router-link>
+    </router-link>-->
   </div>
 </template>
 
@@ -15,13 +49,99 @@
 export default {
   data() {
     return {
-      number: "1",
-      sumOfQuestions: "100"
+      questionNo: "1",
+      sumOfQuestions: "100",
+      // questionsArr: [""],
+      questions: [
+        {
+          technology: "Javascript",
+          text: "What is JS",
+          answer1: "this",
+          answer2: "that",
+          answer3: "sure",
+          answer4: "none",
+          correct: 1
+        },
+        {
+          technology: "GIT",
+          text: "What is GIT",
+          answer1: "I know",
+          answer2: "he knows",
+          answer3: "noone knows",
+          answer4: "who cares",
+          correct: 1
+        },
+        {
+          technology: "SQL",
+          text: "What is SQL",
+          answer1: "null",
+          answer2: "blank",
+          answer3: "0",
+          answer4: "NaN",
+          correct: 1
+        },
+        {
+          technology: "Javascript",
+          text: "What is JS",
+          answer1: "this",
+          answer2: "that",
+          answer3: "sure",
+          answer4: "none",
+          correct: 1
+        },
+        {
+          technology: "GIT",
+          text: "What is GIT",
+          answer1: "I know",
+          answer2: "he knows",
+          answer3: "noone knows",
+          answer4: "who cares",
+          correct: 1
+        },
+        {
+          technology: "SQL",
+          text: "What is SQL",
+          answer1: "null",
+          answer2: "blank",
+          answer3: "0",
+          answer4: "NaN",
+          correct: 1
+        },
+        {
+          technology: "Javascript",
+          text: "What is JS",
+          answer1: "this",
+          answer2: "that",
+          answer3: "sure",
+          answer4: "none",
+          correct: 1
+        },
+        {
+          technology: "GIT",
+          text: "What is GIT",
+          answer1: "I know",
+          answer2: "he knows",
+          answer3: "noone knows",
+          answer4: "who cares",
+          correct: 1
+        },
+        {
+          technology: "SQL",
+          text: "What is SQL",
+          answer1: "null",
+          answer2: "blank",
+          answer3: "0",
+          answer4: "NaN",
+          correct: 1
+        }
+      ]
     };
-  },
-  props: ["technology"]
+  }
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+img {
+  width: 10vw;
+}
 </style>
